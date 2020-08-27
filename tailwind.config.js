4bde13c7f079a7bc80c6c6ff7f7ec2aa3a1be1c9
ADRIAN LARA <adrianlara24@gmail.com>
2020-08-27 17:06:06 -0300

    add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: ['./src/**/*.html', './src/**/*.ts'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2196f2',
+        secondary: '#fec007'
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
